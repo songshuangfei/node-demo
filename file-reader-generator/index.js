27713c866fd7673e8171a5565caa51da72d5298c
songshuangfei <1462499968@qq.com>
2018-12-29 13:29:11 +0800

    run

--- file-reader-generator/index.js
+++ file-reader-generator/index.js
@@ -12,19 +12,29 @@ var fileReader = function(fileName){//读取函数
     });
 };
 
-var readFile = function*(){//读写逻辑
+var writeFilesInOne = function*(){//读写逻辑
     var data1 = yield fileReader("./files/1.txt");
     var data2 = yield fileReader("./files/2.txt");
-    var data = data1.toString() + data2.toString();
-    fs.writeFile("./files/3.txt",data,err => {
+    var data3 = yield fileReader("./files/3.txt");
+    var data4 = yield fileReader("./files/4.txt");
+    var data = data1+data2+data3+data4;
+    fs.writeFile("./files/new.txt",data,err => {
         if(err)
             console.log(err);
     })
 };
 
-var reader = readFile();
-reader.next().value.then((data)=>{//开启读写
-    reader.next(data).value.then(data => {
-        reader.next(data);
-    })
-});
+function run(gen){
+    var g = gen();
+
+    function next(data){
+        var resulet = g.next(data);
+        if(resulet.done) return;
+        resulet.value.then(data =>{
+            next(data);
+        })
+    }
+    next();
+}
+
+run(writeFilesInOne);
